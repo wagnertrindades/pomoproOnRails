6c27e0fe30486c18da44fb5b1ac0bc2a707a2aa2
Wagner Trindade <wtrindades@hotmail.com>
2015-11-23 21:42:46 -0200

    Refactor actions Pomo, Break and Long Break

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -4,4 +4,15 @@ class Timer < ActiveRecord::Base
   validates_inclusion_of :status, :in => %w(pomo break long_break),
     :message => "%{value} não é um tipo valido"
 
+  def is_pomo?
+    return true if self.status == "pomo"
+  end
+
+  def is_break?
+    return true if self.status == "break"
+  end
+
+  def is_long_break?
+    return true if self.status == "long_break"
+  end
 end
