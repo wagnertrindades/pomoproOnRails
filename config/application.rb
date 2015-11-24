9977c5da32a3697097b3ab0f424da97442a53682
Wagner Trindade <wtrindades@hotmail.com>
2015-11-24 18:50:26 -0200

    Added font-icons

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module PomoproOnRails
     
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Config font icons
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
