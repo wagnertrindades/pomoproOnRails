b68741272f20de5b1eb094828834d79317d43e0e
Wagner Trindade <wtrindades@hotmail.com>
2015-11-30 12:42:16 -0200

    Update routes to timer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :path => "accounts" 
 
-  scope path: :users, as: "users" do
+  scope path: :users, as: "user" do
     resources :timers, only: [:create, :index]
   end
 
