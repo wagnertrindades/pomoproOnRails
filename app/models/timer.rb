82caf55f6597acf791d77de71e924e3152034cf8
wagnertrindades <wagner.sanseverin@resultadosdigitais.com.br>
2016-06-05 21:58:49 -0300

    Refactor Timer

--- app/models/timer.rb
+++ app/models/timer.rb
@@ -1,18 +1,25 @@
 class Timer < ActiveRecord::Base
   belongs_to :user
 
-  validates_inclusion_of :status, :in => %w(pomo break long_break),
+  validates_inclusion_of :status,
+    :in => %w(pomo break long_break),
     :message => "%{value} não é um tipo valido"
 
   def is_pomo?
-    return true if self.status == "pomo"
+    status_equals? "pomo"
   end
 
   def is_break?
-    return true if self.status == "break"
+    status_equals? "break"
   end
 
   def is_long_break?
-    return true if self.status == "long_break"
+    status_equals? "long_break"
+  end
+
+  private
+
+  def status_equals?(value)
+    return true if self.status == value
   end
 end
