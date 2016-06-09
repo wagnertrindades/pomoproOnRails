dc8776719450d1324b579c0dcd476a6ebc609da4
wagnertrindades <wagner.sanseverin@resultadosdigitais.com.br>
2016-06-09 08:04:58 -0300

    Add tests to index in Timers Controller

--- spec/controllers/timers_controller_spec.rb
+++ spec/controllers/timers_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe TimersController, type: :controller do
 
+  describe 'GET index' do
+    let(:user) { create(:user) }
+
+    before { sign_in :user, user }
+
+    it 'assigns @user' do
+      get :index, user_id: user.id
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it 'assigns @timers' do
+      timer = create(:timer, :user => user)
+      get :index, user_id: user.id
+      expect(assigns(:timers)).to eq([timer])
+    end
+
+    it 'renders the index template' do
+      get :index, user_id: user.id
+      expect(response).to render_template("index")
+    end
+
+  end
+
+  describe 'POST create'
 end
