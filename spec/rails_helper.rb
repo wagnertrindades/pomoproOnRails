774e9f0b5c95f63c8ce5efcd099362807209dd3b
wagnertrindades <wagner.sanseverin@resultadosdigitais.com.br>
2016-06-15 23:26:27 -0300

    Add gem factory girls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,8 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include DeviseHelpers, type: :feature
+  config.include ControllerHelpers, type: :controller
+  config.include FactoryGirl::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
