4ba247ccd948f2aab37623d1b992a6e5074c686b
wagnertrindades <wagner.sanseverin@resultadosdigitais.com.br>
2016-06-17 23:18:28 -0300

    Add specs to create in Timer Controller and ajust indentation to two spaces

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -1,25 +1,25 @@
 class TimersController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-        @user = current_user
-        @timers = @user.timer.where(created_at: (Time.now.at_beginning_of_day)..Time.now.at_end_of_day)
-        #@timers = @user.timer.all
-        @timer = current_user.timer.new
+  def index
+    @user = current_user
+    @timers = @user.timer.where(created_at: (Time.now.at_beginning_of_day)..Time.now.at_end_of_day)
+    #@timers = @user.timer.all
+    @timer = current_user.timer.new
+  end
+
+  def create
+    @timer = current_user.timer.new(:status => params[:status], :user_id => params[:user_id])
+
+    if @timer.save
+      respond_to do |format|
+        format.html { redirect_to user_timers_path }
+        format.js # render timers/create.js.erb
+      end
+    else
+      flash[:error] = "Problema em concluir timer."
+      redirect_to user_timers_path
     end
-
-    def create
-        @timer = current_user.timer.new(:status => params[:status], :user_id => params[:user_id])
-
-        if @timer.save
-            respond_to do |format|
-                format.html { redirect_to user_timers_path }
-                format.js # render timers/create.js.erb
-            end
-        else
-            flash[:error] = "Problema em concluir timer."
-            render user_timers_path
-        end
-    end
+  end
 
 end
--- spec/controllers/timers_controller_spec.rb
+++ spec/controllers/timers_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe TimersController, type: :controller do
+  let(:user) { create(:user) }
+
+  before { sign_in :user, user }
 
   describe 'GET index' do
-    let(:user) { create(:user) }
-
-    before { sign_in :user, user }
 
     it 'assigns @user' do
       get :index, user_id: user.id
@@ -30,5 +30,48 @@ RSpec.describe TimersController, type: :controller do
 
   end
 
-  describe 'POST create'
+  describe 'POST create' do
+
+    it 'assigns @timer' do
+      post :create, user_id: user.id, status: "pomo"
+      expect(assigns(:timer)).to be_an_instance_of Timer
+    end
+
+    context "has save timer" do
+
+      context "success" do
+
+        it "renders user timers" do
+          post :create, user_id: user.id, status: "pomo"
+          expect(@reponse).to redirect_to user_timers_path
+        end
+
+        it "create timer" do
+          post :create, user_id: user.id, status: "pomo"
+          expect(user.timer).not_to be_empty
+        end
+
+      end
+
+      context "failure" do
+
+        before do
+          allow_any_instance_of(Timer).to receive(:save).and_return false
+        end
+
+        it "renders a failure message" do
+          post :create, user_id: user.id, status: "pomo"
+          expect(flash[:error]).to eq "Problema em concluir timer."
+        end
+
+        it "renders user timers" do
+          post :create, user_id: user.id, status: "pomo"
+          expect(@response).to redirect_to user_timers_path
+        end
+
+      end
+
+    end
+
+  end
 end
